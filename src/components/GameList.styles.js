5fba6e5e13f942cfd4aed21704fe98479e62c6be
beamb <beamb0694@gmail.com>
2026-02-03 22:42:48 -0300

    fix(gamelist): use object-fit: contain for thumbnails to avoid cropping

--- src/components/GameList.styles.js
+++ src/components/GameList.styles.js
@@ -45,7 +45,9 @@ const styles = {
   thumb: {
     width: "100%",
     height: "100%",
-    objectFit: "cover",
+    objectFit: "contain",
+    objectPosition: "center",
+    background: "#000",
     display: "block",
   },
   title: {
